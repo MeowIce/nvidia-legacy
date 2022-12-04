224d01968a3342f3d9d1cf87e65d982d49e9b612
MeowIce <nguyenanhuy2208@gmail.com>
2022-12-04 21:57:23 +0700

    Added blacklust file :3

---
+++ blacklist.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Blacklist nouveau driver to install nVIDIA driver"
+echo "Script by MeowIce"
+echo "Follow me on Github: @MeowIce"
+echo "Now working..."
+bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+update-initramfs -u
+sleep 2
+echo "Now it is safe to restart you computer ;)"
