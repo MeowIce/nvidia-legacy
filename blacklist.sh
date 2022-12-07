25031d0158088b197478de57aa411cfc9156c291
MeowIce <nguyenanhuy2208@gmail.com>
2022-12-07 23:11:29 +0700

    Updated blacklist file :3

--- blacklist.sh
+++ blacklist.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-echo "Blacklist nouveau driver to install nVIDIA driver"
-echo "Script by MeowIce"
-echo "Follow me on Github: @MeowIce"
-echo "Now working..."
-bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-update-initramfs -u
-sleep 2
-echo "Now it is safe to restart you computer ;)"
+if [ "$(whoami)" = "root" ]; then
+	echo "Blacklist nouveau driver to install nVIDIA driver"
+	echo "Script by MeowIce"
+	echo "Follow me on Github: @MeowIce"
+	echo "Now working..."
+	bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+	bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+echo "*** The blacklist file has been written to disk. Please update your ramdisk then restart. ***"
+else
+	echo "*** Please run this script as root. ***"
+fi
 
